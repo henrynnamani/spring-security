f292459ea590fe5e99d2f02f3e83cc253561ed6f
gr4yx01 <hoyx0101@gmail.com>
2026-01-19 09:48:11 +0100

    feat: changed security filter chain

---
+++ src/main/java/com/graey/spring_security/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.graey.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+        http.csrf(csrf-> csrf.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("graey").password("pyr@hornet0101").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin@568").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
